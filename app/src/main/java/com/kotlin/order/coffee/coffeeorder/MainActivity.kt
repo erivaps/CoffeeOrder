b0f0d23d1677416a996472570af8808fb56b6cf1
Erison <erivaps@gmail.com>
2018-10-05 20:50:40 -0500

    first commit

--- app/src/main/java/com/kotlin/order/coffee/coffeeorder/MainActivity.kt
+++ app/src/main/java/com/kotlin/order/coffee/coffeeorder/MainActivity.kt
@@ -2,11 +2,23 @@ package com.kotlin.order.coffee.coffeeorder
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun ordenarTaza(view: View){
+        var txtNroTazas: TextView = findViewById<TextView>(R.id.txtNroRazas)
+
+        var resultado :TextView = findViewById<TextView>(R.id.resultado)
+
+        resultado.setText("Usted ordenó ${txtNroRazas.text} taza, pinche cabron.")
+
     }
 }
